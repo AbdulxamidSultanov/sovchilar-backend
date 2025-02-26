b077dbb4cc8cbd167f877a058004065b04799a0c
itsforme <sultanovabdulxamid@gmail.com>
2025-02-26 23:56:44 +0500

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from aiogram import Bot, Dispatcher, types, filters
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram.filters import Command  # Новый путь в aiogram 3.x
+from aiogram.filters import Command  # Aiogram 3.x
 import asyncio
+from fastapi import FastAPI
 
-TOKEN = "8066095257:AAG9W4w2nyg6WKB7zHt1f-CQ8mtQJPis2wM"
-WEB_APP_URL = "https://sovchilar-tgapp.vercel.app"  # Замени на свою ссылку
+app = FastAPI()  # Render ищет именно эту переменную
+
+@app.get("/")
+async def root():
+    return {"message": "Бот работает!"}
+
+TOKEN = "ТВОЙ_ТОКЕН"
+WEB_APP_URL = "https://sovchilar-tgapp.vercel.app"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -14,11 +21,8 @@ async def start_command(message: types.Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Открыть веб-приложение", web_app=WebAppInfo(url=WEB_APP_URL))]
     ])
-
     await message.answer("Нажмите кнопку ниже, чтобы открыть веб-приложение:", reply_markup=keyboard)
 
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+# Запускаем бота фоновым процессом
+loop = asyncio.get_event_loop()
+loop.create_task(dp.start_polling(bot))
